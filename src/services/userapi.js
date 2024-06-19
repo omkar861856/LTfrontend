447b4004d223b57080976a628ffe03fd64b8697e
Omkar <Omkar861856@gmail.com>
2024-06-20 00:13:48 +0530

    demo user

--- src/services/userapi.js
+++ src/services/userapi.js
@@ -1,3 +1,3 @@
-export const user_api = 'http://localhost:4000'
+export const user_api = 'https://ltbackend2.vercel.app'
 export const socket_user_api = "//learnmore-backend-chi.vercel.app"
-export const enquiry_api = 'http://localhost:4000'
+export const enquiry_api = 'https://ltbackend2.vercel.app'
